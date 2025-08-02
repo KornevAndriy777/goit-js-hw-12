f61065a0038f35dc17218069eb272f343f476e0c
KornevAndriy777 <krnvandriy@gmail.com>
2025-08-03 01:17:45 +0300

    1

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+    form: document.querySelector(".form"),
+    input: document.querySelector('input[name="search-text"]'),
+    gallery: document.querySelector(".gallery"),
+    loader: document.querySelector(".loader"),
+    loadBtn: document.querySelector(".load-more-btn"),
+    scrollBtn: document.querySelector('.scroll-to-top'),
+    header: document.querySelector("header"),
+}
